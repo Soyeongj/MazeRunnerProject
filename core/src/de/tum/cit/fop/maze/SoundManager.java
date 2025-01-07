55e1f48ee622223cd747eb027bb2049c355be74a
Soyeongj <go57mab@tum.de>
2025-01-07 13:24:05 +0100

    lostfrindsound added

--- core/src/de/tum/cit/fop/maze/SoundManager.java
+++ core/src/de/tum/cit/fop/maze/SoundManager.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.audio.Sound;
 
 public class SoundManager {
     private static Sound rockSound;
+    private static Sound lostFreindSound;
 
     // 초기화: 필요한 모든 사운드 로드
     public static void initialize() {
         rockSound = Gdx.audio.newSound(Gdx.files.internal("assets/rockSound.mp3"));
+        lostFreindSound = Gdx.audio.newSound(Gdx.files.internal("assets/lostFriendSound.mp3"));
     }
 
     // 돌 소리 재생
@@ -18,10 +20,19 @@ public class SoundManager {
         }
     }
 
+    public static void playLostFriendSound() {
+        if (lostFreindSound != null) {
+            lostFreindSound.play();
+        }
+    }
+
     // 리소스 해제
     public static void dispose() {
         if (rockSound != null) {
             rockSound.dispose();
+        }
+        if (lostFreindSound != null) {
+            lostFreindSound.dispose();
         }
     }
 }
